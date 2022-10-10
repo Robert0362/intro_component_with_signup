46a6bc70574dc05dd80a11770f3436467a9bb027
Robert Schwindler <robertmichaelschwindler@gmail.com>
2022-10-10 06:47:27 -0400

    Layout

---
+++ scripts/javascript.js
@@ -0,0 +1,9 @@
+const btnClaim = document.getElementById('claim');
+const fName = document.getElementById('fName');
+const lName = document.getElementById('lName');
+const email = document.getElementById('email');
+const passWord = document.getElementById('pWard');
+
+btnClaim.addEventListener('click', function(){
+    console.log('hello');
+});
